be1d519d8f493285e01b3ccc44747053817b55c7
Simons <liyulinga@qq.com>
2018-09-04 18:26:02 +0800

    New: 增加部分skip list代码

--- src/sklist/sklist.cpp
+++ src/sklist/sklist.cpp
@@ -1,9 +1,121 @@
-#include <iostream>
 #include "sklist.h"
 
-using namespace std;
-int main()
+
+SkList::SkList(uint32_t level)
 {
-    cout << "hello world " << endl;
-    return 0;
+    m_max_level = level > 0 ? level : 1;
+    m_head = new SkListNode;
+    m_tail = new SkListNode;
+
+    m_head->m_next_nodes = new SkListNode *[m_max_level];
+    for (int i = 0; i < m_max_level; i++) {
+        m_head->m_next_nodes[i] = m_tail;
+    }
 }
+
+SkList::~SkList(void)
+{
+    SkListNode *curr = nullptr;
+    while (m_head->m_next_nodes[0] != m_tail) {
+        curr = m_head->m_next_nodes[0];
+        m_head->m_next_nodes[0] = curr->m_next_nodes;
+        delete curr->m_next_nodes;
+        delete curr;
+    }
+
+    delete m_head->m_next_nodes;
+    delete m_head;
+    delete m_tail;
+}
+
+void SkList::Insert(uint32_t key, void *data)
+{
+    SkListNode *curr = m_head;
+    SkListNode *update[m_max_level];
+
+    //Lookup location
+    for (int i = m_max_level - 1; i >= 0; --i) {
+        if (curr->m_next_nodes[i] == m_tail || curr->m_next_nodes->m_key > key)
+            update[i] = curr;
+        else {
+            while(curr->m_next_nodes[i] != m_tail && curr->m_next_nodes->m_key < key) {
+                curr = curr->m_next_nodes;
+            }
+            if (curr->m_next_nodes[i] != m_tail && curr->m_next_nodes->m_key == key) {
+                curr->m_next_nodes[i]->m_data = data;
+                return;
+            }
+            update[i] = curr;
+        }
+    }
+
+    //Generate node
+    int level = ::RandomLevel();
+    SkListNode *temp = new SkListNode;
+    temp->m_key = key;
+    temp->m_data = data;
+    temp->m_level = level;
+    temp->m_next_nodes = new SkListNode *[level + 1];
+
+    //Insert node on every level
+    for (int i = 0; i <= level; i++) {
+        temp->m_next_nodes[i] = update[i]->m_next_nodes[i];
+        update[i]->m_next_nodes[i] = temp;
+    }
+
+    return;
+}
+void SkList::Delete(uint32_t key)
+{
+    SkListNode *curr = m_head;
+    SkListNode *update[m_max_level];
+
+    //Lookup location
+    for (int i = m_max_level - 1; i >= 0; --i) {
+        if (curr->m_next_nodes[i] == m_tail || curr->m_next_nodes->m_key > key)
+            update[i] = curr;
+        else {
+            while(curr->m_next_nodes[i] != m_tail && curr->m_next_nodes->m_key < key) {
+                curr = curr->m_next_nodes;
+            }
+            if (curr->m_next_nodes[i] != m_tail && curr->m_next_nodes->m_key == key) {
+                curr->m_next_nodes[i]->m_data = data;
+                return;
+            }
+            update[i] = curr;
+        }
+    }
+
+
+}
+void *SkList::Get(uint32_t key)
+{
+    SkListNode *curr = m_head;
+
+    //Lookup location
+    for (int i = m_max_level - 1; i >= 0; --i) {
+        if (curr->m_next_nodes[i] == m_tail || curr->m_next_nodes->m_key > key)
+            update[i] = curr;
+        else {
+            while(curr->m_next_nodes[i] != m_tail && curr->m_next_nodes->m_key < key) {
+                curr = curr->m_next_nodes;
+            }
+            if (curr->m_next_nodes[i] != m_tail && curr->m_next_nodes->m_key == key) {
+                return curr->m_next_nodes[i]->m_data;
+            }
+            update[i] = curr;
+        }
+    }
+    return nullptr;
+}
+
+void SkList::Display(void)
+{
+
+}
+
+int32_t SkList::RandomLevel(void)
+{
+
+}
+
--- src/sklist/sklist.h
+++ src/sklist/sklist.h
@@ -1,8 +1,30 @@
 #ifndef __SKLIST__
 #define __SKLIST__
 
+#include <stdint.h>
+
+typedef struct SkListNode {
+    uint32_t m_key;
+    uint32_t m_level;
+    void *m_data;
+    SkListNode **m_next_nodes;
+} SkListNode;
 
 
+class SkList {
+    public:
+        SkList(uint32_t level);
+        ~SkList(void);
+        void Insert(uint32_t key, void *data);
+        void Delete(uint32_t key);
+        void *Get(uint32_t key);
+        void Display(void);
+    private:
+        int32_t m_max_level;
+        int32_t RandomLevel(void);
+        SkListNode *m_head;
+        SkListNode *m_tail;
+};
 
 
 #endif // __SKLIST__
