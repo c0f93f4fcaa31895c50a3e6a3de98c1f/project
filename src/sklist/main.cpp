4f5940b953515e08aaab26bc6042fb1f3a0026b3
Simons <liyulinga@qq.com>
2018-09-05 11:17:23 +0800

    Change: skip list test OK

--- src/sklist/main.cpp
+++ src/sklist/main.cpp
@@ -2,8 +2,30 @@
 #include "sklist.h"
 
 using namespace std;
+
+//Just for test
+void *GenData(int len) {
+    void *ptr = new char [len];
+
+    return ptr;
+}
+
 int main()
 {
-    cout << "hello world " << endl;
+    SkList *sklist = new SkList(3);
+
+    sklist->Insert(1, GenData(3));
+    sklist->Insert(2, GenData(2));
+    sklist->Insert(3, GenData(4));
+    sklist->Insert(4, GenData(5));
+    sklist->Insert(5, GenData(5));
+    sklist->Insert(6, GenData(3));
+    sklist->Insert(7, GenData(2));
+    sklist->Insert(8, GenData(4));
+    sklist->Insert(9, GenData(5));
+    sklist->Insert(10, GenData(5));
+
+    sklist->Display();
     return 0;
 }
+
--- src/sklist/sklist.cpp
+++ src/sklist/sklist.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
 #include "sklist.h"
 
+
+using namespace std;
 
 SkList::SkList(uint32_t level)
 {
@@ -18,7 +21,7 @@ SkList::~SkList(void)
     SkListNode *curr = nullptr;
     while (m_head->m_next_nodes[0] != m_tail) {
         curr = m_head->m_next_nodes[0];
-        m_head->m_next_nodes[0] = curr->m_next_nodes;
+        m_head->m_next_nodes[0] = curr->m_next_nodes[0];
         delete curr->m_next_nodes;
         delete curr;
     }
@@ -35,13 +38,13 @@ void SkList::Insert(uint32_t key, void *data)
 
     //Lookup location
     for (int i = m_max_level - 1; i >= 0; --i) {
-        if (curr->m_next_nodes[i] == m_tail || curr->m_next_nodes->m_key > key)
+        if (curr->m_next_nodes[i] == m_tail || curr->m_next_nodes[i]->m_key > key)
             update[i] = curr;
         else {
-            while(curr->m_next_nodes[i] != m_tail && curr->m_next_nodes->m_key < key) {
-                curr = curr->m_next_nodes;
+            while(curr->m_next_nodes[i] != m_tail && curr->m_next_nodes[i]->m_key < key) {
+                curr = curr->m_next_nodes[i];
             }
-            if (curr->m_next_nodes[i] != m_tail && curr->m_next_nodes->m_key == key) {
+            if (curr->m_next_nodes[i] != m_tail && curr->m_next_nodes[i]->m_key == key) {
                 curr->m_next_nodes[i]->m_data = data;
                 return;
             }
@@ -50,7 +53,7 @@ void SkList::Insert(uint32_t key, void *data)
     }
 
     //Generate node
-    int level = ::RandomLevel();
+    int level = RandomLevel();
     SkListNode *temp = new SkListNode;
     temp->m_key = key;
     temp->m_data = data;
@@ -65,6 +68,7 @@ void SkList::Insert(uint32_t key, void *data)
 
     return;
 }
+
 void SkList::Delete(uint32_t key)
 {
     SkListNode *curr = m_head;
@@ -72,38 +76,51 @@ void SkList::Delete(uint32_t key)
 
     //Lookup location
     for (int i = m_max_level - 1; i >= 0; --i) {
-        if (curr->m_next_nodes[i] == m_tail || curr->m_next_nodes->m_key > key)
+        if (curr->m_next_nodes[i] == m_tail || curr->m_next_nodes[i]->m_key > key)
             update[i] = curr;
         else {
-            while(curr->m_next_nodes[i] != m_tail && curr->m_next_nodes->m_key < key) {
-                curr = curr->m_next_nodes;
+            while(curr->m_next_nodes[i] != m_tail && curr->m_next_nodes[i]->m_key < key) {
+                curr = curr->m_next_nodes[i];
             }
-            if (curr->m_next_nodes[i] != m_tail && curr->m_next_nodes->m_key == key) {
-                curr->m_next_nodes[i]->m_data = data;
-                return;
+            if (curr->m_next_nodes[i] != m_tail && curr->m_next_nodes[i]->m_key == key) {
+                update[i] = curr;
+            } else {
+                update[i] = nullptr;
             }
-            update[i] = curr;
         }
     }
 
+    SkListNode *temp = nullptr;
 
+    for (int i = 0; i < m_max_level; ++i) {
+       if (update[i]) {
+            temp = update[i]->m_next_nodes[i];
+            update[i]->m_next_nodes[i] = temp->m_next_nodes[i];
+       }
+    }
+
+    if (temp) {
+        delete temp->m_next_nodes;
+        delete temp;
+    }
+    return;
 }
+
 void *SkList::Get(uint32_t key)
 {
     SkListNode *curr = m_head;
 
     //Lookup location
     for (int i = m_max_level - 1; i >= 0; --i) {
-        if (curr->m_next_nodes[i] == m_tail || curr->m_next_nodes->m_key > key)
-            update[i] = curr;
+        if (curr->m_next_nodes[i] == m_tail || curr->m_next_nodes[i]->m_key > key)
+            continue;
         else {
-            while(curr->m_next_nodes[i] != m_tail && curr->m_next_nodes->m_key < key) {
-                curr = curr->m_next_nodes;
+            while(curr->m_next_nodes[i] != m_tail && curr->m_next_nodes[i]->m_key < key) {
+                curr = curr->m_next_nodes[i];
             }
-            if (curr->m_next_nodes[i] != m_tail && curr->m_next_nodes->m_key == key) {
+            if (curr->m_next_nodes[i] != m_tail && curr->m_next_nodes[i]->m_key == key) {
                 return curr->m_next_nodes[i]->m_data;
             }
-            update[i] = curr;
         }
     }
     return nullptr;
@@ -111,11 +128,23 @@ void *SkList::Get(uint32_t key)
 
 void SkList::Display(void)
 {
+    SkListNode *curr = m_head;
 
+    for (int i = m_max_level - 1; i >= 0; --i) {
+        curr = m_head;
+        while (curr->m_next_nodes[i] != m_tail) {
+            curr = curr->m_next_nodes[i];
+            cout << "curr level[" << i << "] key[ " << curr->m_key << "]" << endl;
+        }
+    }
 }
 
 int32_t SkList::RandomLevel(void)
 {
+    int level = 0;
+    while (rand() % 2 && level < m_max_level - 1)
+        ++level;
 
+    return level;
 }
 
